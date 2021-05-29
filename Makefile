2bb366985e387f3efdd49ae7b3d14385ea68b4f7
Rainer Poisel <rainer.poisel@gmail.com>
2021-05-29 20:57:55 +0200

    chore: improved readability of i2c-eg

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-all: cmd/modbus-mqtt/modbus-mqtt cmd/joyblind/joyblind cmd/loxone-proxy/loxone-proxy cmd/mqtt-db-postgres/mqtt-db-postgres
+all: cmd/modbus-mqtt/modbus-mqtt \
+	cmd/joyblind/joyblind \
+	cmd/loxone-proxy/loxone-proxy \
+	cmd/mqtt-db-postgres/mqtt-db-postgres \
+	cmd/i2c-eg/i2c-eg
 
 cmd/modbus-mqtt/modbus-mqtt: cmd/modbus-mqtt/main.go internal/util/util.go
 	(cd cmd/modbus-mqtt && env GOOS=linux GOARCH=arm GOARM=7 go build -v -mod=vendor)
@@ -12,10 +16,14 @@ cmd/loxone-proxy/loxone-proxy: cmd/loxone-proxy/main.go internal/util/util.go
 cmd/mqtt-db-postgres/mqtt-db-postgres: cmd/mqtt-db-postgres/main.go internal/util/util.go
 	(cd cmd/mqtt-db-postgres && env GOOS=linux GOARCH=arm GOARM=7 go build -v -mod=vendor)
 
+cmd/i2c-eg/i2c-eg: cmd/i2c-eg/main.go
+	(cd cmd/i2c-eg && env GOOS=linux GOARCH=arm GOARM=7 go build -v -mod=vendor)
+
 .PHONY: clean
 clean:
 	-rm \
 		cmd/modbus-mqtt/modbus-mqtt \
 		cmd/joyblind/joyblind \
 		cmd/loxone-proxy/loxone-proxy \
-		cmd/mqtt-db-postgres/mqtt-db-postgres
+		cmd/mqtt-db-postgres/mqtt-db-postgres \
+		cmd/i2c-eg/i2c-eg
--- cmd/i2c-eg/main.go
+++ cmd/i2c-eg/main.go
@@ -8,39 +8,84 @@ import (
 	"github.com/d2r2/go-logger"
 )
 
+const (
+	inputBadLicht = 1 << iota
+	inputStiegeLicht
+	inputKuecheLichtZeile
+	input3
+	input4
+	input5
+	input6
+	input7
+)
+
+const (
+	outputKlingel = 1 << iota
+	outputStiegeLichtOben
+	outputStiegeLichtUnten
+	outputKuecheLichtZeile
+	unused0
+	unused1
+	unused2
+	unused3
+)
+
+const (
+	i2haAddr = 0x20
+	i2heAddr = 0x38
+)
+
+func toggle(data *byte, mask byte) {
+	(*data) ^= mask
+}
+
+func isBitsSet(data byte, mask byte) bool {
+	return data&mask == mask
+}
+
+func isBitsUnset(data byte, mask byte) bool {
+	return data&mask == 0x00
+}
+
 func main() {
 	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
 
-	i2ha, err := goi2c.NewI2C(0x20, 1)
+	i2ha, err := goi2c.NewI2C(i2haAddr, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer i2ha.Close()
-	i2he, err := goi2c.NewI2C(0x38, 1)
+	i2he, err := goi2c.NewI2C(i2heAddr, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer i2he.Close()
 
-	inputs := []byte{0x00}
-	outputs := []byte{0xff}
+	inputs := byte(0x00)
+	outputs := byte(0xff)
 	firstTime := true
 	for {
 		select {
 		case <-time.After(time.Millisecond * 50):
-			curInputs := []byte{0x00}
-			if _, err := i2he.ReadBytes(curInputs); err != nil {
-				log.Printf("Could read from I2C device: %w\n", err)
+			inputData := []byte{0x00}
+			if _, err := i2he.ReadBytes(inputData); err != nil {
+				log.Printf("Could read from I2C device: %s\n", err)
 			}
+			curInputs := inputData[0]
+
 			if firstTime {
 				firstTime = false
 			} else {
-				if curInputs[0]&0x02 == 0x00 && inputs[0]&0x02 == 0x02 {
-					outputs[0] ^= 0x06
+				if isBitsUnset(curInputs, inputStiegeLicht) && isBitsSet(inputs, inputStiegeLicht) {
+					toggle(&outputs, outputStiegeLichtOben)
+					toggle(&outputs, outputStiegeLichtUnten)
 				}
-				_, err := i2ha.WriteBytes(outputs)
+				if isBitsUnset(curInputs, inputKuecheLichtZeile) && isBitsSet(inputs, inputKuecheLichtZeile) {
+					toggle(&outputs, outputKuecheLichtZeile)
+				}
+				_, err := i2ha.WriteBytes([]byte{outputs})
 				if err != nil {
-					log.Printf("Could not write to I2C device: %w\n", err)
+					log.Printf("Could not write to I2C device: %s\n", err)
 				}
 			}
 			inputs = curInputs
